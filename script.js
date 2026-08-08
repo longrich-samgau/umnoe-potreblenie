3d165fe32af6ce028417bee622bfbecf8fd6506f
Nuripa_Longrich <nuripaajtbaeva@gmail.com>
2026-08-08 21:23:12 +0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.cta')?.addEventListener('click', function () {
-  const target = document.querySelector('#next');
-  if (target) target.scrollIntoView({ behavior: 'smooth', block: 'start' });
-});
